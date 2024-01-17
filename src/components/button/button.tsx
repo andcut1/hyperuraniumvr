e77d5f91d6e686d6cf727e89b0ae16cbe5de3342
Andrea Cutolo <acutolo@taal.it>
2024-01-17 18:08:40 +0100

    added ancor component and style for buttons

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,5 @@
+import './button.css'
+
 type ButtonProps = {
     label: string
     outlined?: boolean
@@ -5,14 +7,17 @@ type ButtonProps = {
 }
 
 function Button({ label, outlined, className }: ButtonProps) {
-    const filledClass = `bg-customPrimary border-2 border-customPrimary w-48 py-5 transition-all delay-100 hover:bg-customSecondary hover:border-customSecondary ${className}`
-    const outlinedClass = `bg-transparent border-2 border-customPrimary w-48 py-5 transition-all delay-100 hover:bg-customPrimary  ${className}`
+    const filledClass = `text-white relative py-5 w-48 bg-customPrimary overflow-hidden px-3 transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-customSecondary before:transition-all before:duration-200 hover:text-white hover:before:left-0 hover:before:bg-customSecondary hover:before:w-full clipPath ${className}`
+    const outlinedClass = `text-white relative py-5 w-48 border-2 border-customPrimary bg-transparent overflow-hidden px-3 transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-customPrimary before:transition-all before:duration-200 hover:text-white hover:before:left-0 hover:before:bg-customPrimary hover:before:w-full clipPath ${className}`
     return (
-        <button className={outlined ? outlinedClass : filledClass}>
-            <span className='text-white'>
-                {label}
-            </span>
-        </button>
+        <>
+            <button className={outlined ? outlinedClass : filledClass}>
+                <span className='relative z-10'>
+                    {label}
+                </span>
+            </button>
+            {outlined ? <span className="squareAngle"></span> : null}
+        </>
     )
 }
 
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import NavIcon from '../navbar/nav-icon'
 import Button from '../button/button'
+import AncorHover from '../ancor-hover/ancor-hover'
+import { Flip } from 'react-awesome-reveal'
 
 function Footer() {
     return (
         <div className="bg-black">
             <div className="flex h-32 items-center justify-around">
                 <NavIcon />
-                <a href="" className="bg-transparent uppercase p-3 text-white">main home</a>
-                <a href="" className="bg-transparent uppercase p-3 text-white">boosting</a>
-                <a href="" className="bg-transparent uppercase p-3 text-white">esports</a>
-                <a href="" className="bg-transparent uppercase p-3 text-white">gamer</a>
-                <a href="" className="bg-transparent uppercase p-3 text-white">magazine</a>
-                <Button label='OUTLINED' className='mb-5 sm:m-0' outlined />
+                <AncorHover label='home' />
+                <AncorHover label='boosting' />
+                <AncorHover label='esports' />
+                <AncorHover label='gamer' />
+                <AncorHover label='magazine' />
+                <Flip direction='horizontal' >
+                    <Button label='OUTLINED' className='mb-5 sm:m-0' outlined />
+                </Flip>
             </div>
         </div>)
 }
